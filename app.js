ab49309e7e42243835b6af3804964a15843153c0
Roman Popovych <popovychjob@gmail.com>
2024-06-21 08:32:52 +0300

    Complete

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ import 'dotenv/config'
 import express from "express"
 import mongoose from 'mongoose'
 import cors from 'cors'
+import userRouter from './routes/userRouter.js'
+import taskRouter from './routes/taskRouter.js'
+import testRouter from './routes/testRouter.js'
 
 const app = express()
 
 app.use(cors({
-	origin: 'http://localhost:3000',
+	origin: '*',
 	credentials: true,
 }))
+
 app.use(express.json())
+app.use("/user", userRouter)
+app.use("/task", taskRouter)
+app.use("/test", testRouter)
 
 async function main() {
 	await mongoose.connect(process.env.DB_CONNECT + process.env.DB_NAME)
